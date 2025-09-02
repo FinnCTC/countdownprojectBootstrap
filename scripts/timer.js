2046fc927cb2e645e7764d2907584fe8fe5a6902
FinnCTC <finnmayberryctc@gmail.com>
2025-09-02 14:45:06 -0500

    Update timer.js

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,5 +1,5 @@
 /* set the date for timer end */
-var endDate = new Date("May 1, 2026 12:00:00").getTime();
+var endDate = new Date("December 18, 2026 12:00:00").getTime();
 
 /* update count every second */
 var x = setInterval(function() {
